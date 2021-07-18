1973c625121bcfae285a617baf579f8a39a7cbe2
Jatin Dhar <jatindhar350@gmail.com>
2021-07-18 20:47:12 +0530

    Create 09 Create a Component with Composition.jsx

---
+++ React/09 Create a Component with Composition.jsx
@@ -0,0 +1,26 @@
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+        { /* Change code below this line */ }
+// Change Code Here
+<ChildComponent />
+// Change Code Here
+
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
