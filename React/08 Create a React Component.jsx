96ec46ed40e261cdd52517722b2d782f42d3bd35
Jatin Dhar <jatindhar350@gmail.com>
2021-07-18 20:32:13 +0530

    Create 08 Create a React Component.jsx

---
+++ React/08 Create a React Component.jsx
@@ -0,0 +1,18 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // Change code below this line
+return (
+// Change Code Here
+  <div>
+  <h1>Hello React!</h1>
+  </div>
+// Change Code Here
+)
+
+
+    // Change code above this line
+  }
+};
