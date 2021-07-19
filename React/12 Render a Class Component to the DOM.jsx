baa67c403145afb239a0c35bc132ddeabd324155
Jatin Dhar <jatindhar350@gmail.com>
2021-07-19 20:19:10 +0530

    Create 12 Render a Class Component to the DOM.jsx

---
+++ React/12 Render a Class Component to the DOM.jsx
@@ -0,0 +1,22 @@
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+// Change Code Here
+ <Fruits />
+ <Vegetables />
+// Change Code Here
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+};
+// Change Code Here
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'))
+// Change Code Here
+// Change code below this line
